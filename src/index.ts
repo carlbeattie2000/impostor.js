bbd3cba6ab68963306af7ba8a624d13b94cdbb4f
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-04 17:51:33 +0100

    feat: expose shipping module

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,44 @@
 import { Base } from "./core/base";
 import locales from "./locales";
-import { FinanceLocale } from "./locales/finance";
-import { InternetLocale } from "./locales/internet";
-import { Locale } from "./locales/locale";
-import { LocationLocale } from "./locales/location";
-import { PersonLocale } from "./locales/person";
-import { PhoneLocale } from "./locales/phone";
-import { VehicleLocale } from "./locales/vehicle";
 import { ColorModule } from "./modules/colors";
 import { CryptoModule } from "./modules/crypto";
 import { ScienceModule } from "./modules/science";
+import ShippingModule from "./modules/shipping/shipping";
 import { TimezoneModule } from "./modules/timezones";
-import { localeCodes } from "./types/locales";
 
-export class Impostor extends Base {
+import type { FinanceLocale } from "./locales/finance";
+import type { InternetLocale } from "./locales/internet";
+import type { Locale } from "./locales/locale";
+import type { LocationLocale } from "./locales/location";
+import type { PersonLocale } from "./locales/person";
+import type { PhoneLocale } from "./locales/phone";
+import type { VehicleLocale } from "./locales/vehicle";
+import type { localeCodes } from "./types/locales";
+
+export default class Impostor extends Base {
   private locale: Locale;
 
   internet: InternetLocale;
+
   finance: FinanceLocale;
+
   location: LocationLocale;
+
   person: PersonLocale;
+
   phone: PhoneLocale;
+
   vehicle: VehicleLocale;
 
   color: ColorModule;
+
   crypto: CryptoModule;
+
   science: ScienceModule;
+
   timezone: TimezoneModule;
+
+  shipping: ShippingModule;
 
   constructor(countryCode: localeCodes = "en_GB") {
     super();
@@ -42,9 +54,11 @@ export class Impostor extends Base {
     this.crypto = new CryptoModule();
     this.science = new ScienceModule();
     this.timezone = new TimezoneModule();
+
+    this.shipping = new ShippingModule();
   }
 
-  setLocale(countryCode: localeCodes) {
+  setLocale(countryCode: localeCodes): void {
     this.locale = locales.get(countryCode);
 
     this.internet = this.locale.internet;
